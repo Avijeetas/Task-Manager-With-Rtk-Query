7abbe9ca55caf90f040304e5d5e66422f9e2c46b
avijeetshil <avijeetshil110@gmail.com>
2023-03-25 14:39:27 +0600

    added codebase

---
+++ assignment/src/pages/Home.js
@@ -0,0 +1,10 @@
+import Sidebar from "../components/sidebar/Sidebar";
+import Navbar from "../components/navbar/Navbar"
+export default function Home(){
+    return (
+        <>
+            <Navbar/>
+            <Sidebar/>
+        </>
+    )
+}
